74ecdcd9e0f8431760ca18263bb5d6b2be5170d5
Surakit Choodet <kuakling@gmail.com>
2017-01-25 22:05:54 +0700

    Create SmartWizardAsset.php

---
+++ SmartWizardAsset.php
@@ -0,0 +1,26 @@
+<?php
+namespace kuakling\smartwizard;
+use yii\web\AssetBundle;
+/**
+ * Asset bundle for the smart wizard files.
+ *
+ * @author kuakling <kuakling@gmail.com>
+ * @since 2.0
+ */
+class SmartWizardAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/smartwizard';
+    
+    public $css = [
+        'css/smart_wizard.css',
+    ];
+    
+    public $js = [
+        'js/jquery.smartWizard.min.js',
+    ];
+    
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
